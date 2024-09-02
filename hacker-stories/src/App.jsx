9e81aca1476b675aaf45e9b8800e55521128625b
Larry Jones <mrwizard82d1@gmail.com>
2024-09-02 11:18:49 -0500

    Render the `title` property of an object list

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,18 +1,30 @@
 import './App.css'
 
-const welcome = {
-  greeting: 'Hey',
-  title : "React (Using Vite)",
-}
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectId: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectId: 1,
+  },
+];
 
 function App() {
-  const makeGreeting = () => {return `${welcome.greeting}, ${welcome.title}`;}
-
-  console.log(makeGreeting());
+  console.log(list);
 
   return (
     <div>
-      <h1>{makeGreeting()}</h1>
+      {list.map((element) => <h1 key={element.objectId}>{element.title}</h1>)}
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
